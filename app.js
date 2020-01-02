7632436b089a799fa111babbb1bcf539955e92ad
braeden123 <braedensmith2@gmail.com>
2020-01-01 23:50:28 -0600

    basic functionality

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const sharp = require('sharp')
+const fetch = require('node-fetch');
+const path = require('path')
+const app = express()
+const port = process.env.PORT || 3000
+app.use(express.static('public'))
+
+app.get('/image', async (req, res) => {
+    console.log(req.query.url)
+
+    try {
+        if (!req.query.url) throw "Error processing image query"
+        const response = await fetch(req.query.url)
+        if (!response.ok || !response.headers.get('content-type').includes("image")) {
+            throw "Content type error"
+        }
+        const image = await response.body
+        res.type('jpeg')
+        const pipeline = sharp()
+        const sharpObject = image.pipe(pipeline)
+        const newWidth = (await sharpObject.metadata()).width / (parseInt(req.query.scale) || 2)
+        const out = image.pipe(pipeline.resize(newWidth).jpeg({
+            quality: parseInt(req.query.qual) || 80
+        }))
+        out.pipe(res)
+    } catch (e) {
+        console.log(e)
+        res.send("Error")
+    }
+})
+
+
+app.listen(port, () => console.log(`Started at port ${port}`))
